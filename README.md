f2426987ef94065b6cac35cc8c059aeafc02f4a0
matsuokashuhei <matsuokashuheiii@gmail.com>
2020-12-12 14:30:19 +0900

    Update for new Macbook
    - Rename Git files
    - Update setup script
    - Add README

---
+++ README.md
@@ -0,0 +1,12 @@
+# Usage
+
+```
+git clone https://github.com/matsuokashuhei/dotfiles.git $HOME/.dotfiles
+./$HOME/.dotfiles/bootstrap.sh
+```
+
+[bootstrap.sh](https://github.com/matsuokashuhei/dotfiles/blob/master/bootstrap.sh) will make following files:
+
+- `$HOME/.config/git/config`
+- `$HOME/.gitconfig` (link `$HOME/.config/git/config`)
+- `$HOME/.config/git/ignore`
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,44 +1,13 @@
 #!/bin/bash -eu
 
-DOTFILES_HOME=.dotfiles
+DOTFILES_HOME=$HOME/.dotfiles
 
-print_header() {
-  printf "\e[34m"
-  echo '------------------------------------------------------------------'
-  echo '                      __      __  _____ __                        '
-  echo '                 ____/ /___  / /_/ __(_) /__  _____               '
-  echo '                / __  / __ \/ __/ /_/ / / _ \/ ___/               '
-  echo '               / /_/ / /_/ / /_/ __/ / /  __(__  )                '
-  echo '               \__,_/\____/\__/_/ /_/_/\___/____/                 '
-  echo '                                                                  '
-  echo '                Harder, Better, Faster, Stronger                  '
-  echo '                                                                  '
-  echo '                  github.com/matsuokashuhei/dotfiles              '
-  echo '                                                                  '
-  echo '------------------------------------------------------------------'
-  printf "\e[0m\n"
-}
+# Git
+mkdir -p $HOME/.config/git
 
-# bash_profile
-DOTFILE=$HOME/.bash_profile
-if [ -f $DOTFILE ];
-then
-  cp $DOTFILE ${DOTFILE}-backup-$(date '+%Y%m%d')
-fi
-ln -fs $DOTFILES_HOME/bash/bash_profile $DOTFILE
+for file in config ignore
+do
+  ln -s $DOTFILES_HOME/git/$file $HOME/.config/git/$file
+done
 
-# gitconfig
-DOTFILE=$HOME/.gitconfig
-if [ -f $DOTFILE ];
-then
-  cp $DOTFILE ${DOTFILE}-backup-$(date '+%Y%m%d')
-fi
-ln -fs $DOTFILES_HOME/git/gitconfig $DOTFILE
-
-# gitignore
-DOTFILE=$HOME/.gitignore
-if [ -f $DOTFILE ];
-then
-  cp $DOTFILE ${DOTFILE}-backup-$(date '+%Y%m%d')
-fi
-ln -fs $DOTFILES_HOME/git/gitignore $DOTFILE
+ln -s $HOME/.config/git/config $HOME/.gitconfig
